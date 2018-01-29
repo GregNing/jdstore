a27fd8032b5aca0addb92a86686ecb569071f1b4
Greg <GregJieNing@gmail.com>
2018-01-30 00:10:44 +0800

    git commit -m "add order controller"

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :products
-
+  has_many :orders
   def admin?
     self.is_admin
   end
