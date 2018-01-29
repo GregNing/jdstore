b955900438892431b882f65ac9d7880d8de56263
Greg <GregJieNing@gmail.com>
2018-01-29 23:05:30 +0800

    change quantity in cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,15 @@ class ProductsController < ApplicationController
        flash[:notice] = "成功加入购物车"
        redirect_to :back
     end
+
+  def add_to_cart
+    @products = Product.find(params[:id])
+   if !current_cart.products.include?(@products)
+     current_cart.add_product_to_cart(@products)
+     flash[:notice] = "你已成功 #{@products.title} 加入購物車"
+   else
+     flash[:warning] = "你的購物車已有此商品"
+   end
+    redirect_to :back
+  end
 end
