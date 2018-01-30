c111c4be67d7016b637eb3fed8e4bef961b8f9f7
Greg <GregJieNing@gmail.com>
2018-01-30 16:04:39 +0800

    carriewave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                  
     config.fog_credentials = {
       provider:              'AWS',                        
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],         
+      aws_access_key_id:     'AKIAJXZ76QUHZ6Q2HSOQ',         
 
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        
+      aws_secret_access_key: 'v6ZSGTxyNaerxntSjuzRoT5TRzvC1U',        
 
-      region:                ENV["AWS_REGION"]    
+      region:                'ap-northeast-1'    
 
     }
-    config.fog_directory  = ENV["AWS_BUCKET_NAME"] 
+    config.fog_directory  = 'fullstackbygreg'
 
 
   else
